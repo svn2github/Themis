60930f9e47bdd1e14592bd1608f9fbb64eb53a90
z3r0_one <z3r0_one@ecaf47ba-bb13-0410-a3b6-899b003a5108>
2001-11-24 01:50:52 +0000

    Slight update to the script.
    git-svn-id: svn://svn.code.sf.net/p/themis/code@46 ecaf47ba-bb13-0410-a3b6-899b003a5108

--- trunk/themis/makelinks.sh
+++ trunk/themis/makelinks.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 rm ./modules/http/plugman.cpp
-cd modules/http
-ln -s ../../framework/plugman.cpp plugman.cpp
-ln -s ../../common/plugclass.cpp plugclass.cpp
-cd ../cache
-ln -s ../../common/plugclass.cpp plugclass.cpp
-cd ../..
+ln -s ../../framework/plugman.cpp ./modules/http/plugman.cpp
+ln -s ../../common/plugclass.cpp ./modules/http/plugclass.cpp
+ln -s ../../common/plugclass.cpp ./modules/cache/plugclass.cpp
+
 
 
