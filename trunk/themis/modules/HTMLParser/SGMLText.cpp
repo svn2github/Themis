d3ec6a74d7829bd53e7ee8a96b2a8ed88fc2c737
mark_hellegers <mark_hellegers@ecaf47ba-bb13-0410-a3b6-899b003a5108>
2003-04-18 12:48:46 +0000

    - Forgot to add these support files. These should also be stable.
    git-svn-id: svn://svn.code.sf.net/p/themis/code@193 ecaf47ba-bb13-0410-a3b6-899b003a5108

---
+++ trunk/themis/modules/HTMLParser/SGMLText.cpp
@@ -0,0 +1,118 @@
+/*	SGMLText implementation
+	See SGMLText.hpp for more information
+*/
+
+// Standard C headers
+#include <stdio.h>
+
+// SGMLParser headers
+#include "SGMLText.hpp"
+#include "ReadException.hpp"
+#include "PositionException.hpp"
+
+SGMLText	::	SGMLText( const string & aText )	{
+	
+	mText = aText;
+	
+	Position textInfo( 0, mText.size(), 1, 1 );
+	mPositions.push_back( textInfo );
+	
+}
+
+SGMLText	::	~SGMLText()	{
+	
+}
+
+void SGMLText	::	addChar( char aChar )	{
+	
+	mText += aChar;
+
+	Position & textInfo = mPositions[ 0 ];
+	textInfo.increaseSize();
+
+}
+
+char SGMLText	::	nextChar()	{
+
+	Position & current = mPositions.back();
+	
+	try	{
+		current.nextPosition( mText );
+	}
+	catch( PositionException p )	{
+		// End of piece of text reached
+		//printf( "Text popped\n" );
+		mPositions.pop_back();
+		if ( mPositions.size() == 0 )	{
+			// End of total text reached
+			throw ReadException( current.getLineNr(), current.getCharNr(),
+											"End of text reached", true, true );
+		}
+		return getChar();
+	}
+
+	return mText[ current.getIndex() ];
+
+}
+
+char SGMLText	::	getChar()	{
+
+	// WARNING: Might be dodgy if a zero length entity was just pushed on the stack
+	
+	Position & current = mPositions.back();
+	
+	return mText[ current.getIndex() ];
+
+}
+
+void SGMLText	::	addEntity( const Position & aEntity )	{
+	
+	//printf( "Adding entity: index %i, lineNr %i, charNr %i\n", aEntity.getIndex(), aEntity.getLineNr(), aEntity.getCharNr() );
+	
+	mPositions.push_back( aEntity );
+
+	//printf( "First character of entity: %c\n", getChar() );
+	
+}
+
+State SGMLText	::	saveState() const	{
+	
+	return State( mPositions );
+	
+}
+
+void SGMLText	::	restoreState( const State & aState )	{
+	
+	mPositions = aState.getPositions();
+	
+}
+
+unsigned int SGMLText	::	getLineNr() const	{
+
+	const Position & current = mPositions.back();
+		
+	return current.getLineNr();
+	
+}
+
+unsigned int SGMLText	::	getCharNr() const	{
+	
+	const Position & current = mPositions.back();
+		
+	return current.getCharNr();
+	
+}
+
+unsigned int SGMLText	::	getIndex() const	{
+	
+	const Position & current = mPositions.back();
+		
+	return current.getIndex();
+	
+}
+
+unsigned int SGMLText	::	getSize() const	{
+	
+	return mText.size();
+	
+}
---
+++ trunk/themis/modules/HTMLParser/SGMLText.hpp
@@ -0,0 +1,43 @@
+/*	SGMLText
+	Stores a piece of raw text from an entity
+
+	Mark Hellegers (M.H.Hellegers@stud.tue.nl)
+	11-04-2003
+*/
+
+#ifndef SGMLTEXT_HPP
+#define SGMLTEXT_HPP
+
+// Standard C++ headers
+#include <string>
+#include <vector>
+
+// SGMLParser headers
+#include "Position.hpp"
+#include "State.hpp"
+
+using namespace std;
+
+class SGMLText	{
+	
+	private:
+		string mText;
+		vector<Position> mPositions;
+		
+	public:
+		SGMLText( const string & aText = "" );
+		~SGMLText();
+		void addChar( char aChar );
+		char nextChar();
+		char getChar();
+		void addEntity( const Position & aEntity );
+		State saveState() const;
+		void restoreState( const State & aState );
+		unsigned int getIndex() const;
+		unsigned int getLineNr() const;
+		unsigned int getCharNr() const;
+		unsigned int getSize() const;
+
+};
+
+#endif
