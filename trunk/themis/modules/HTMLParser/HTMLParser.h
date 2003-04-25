dcec6d4a16d7708dfb876ac789d8c4bea8bfa4a5
mark_hellegers <mark_hellegers@ecaf47ba-bb13-0410-a3b6-899b003a5108>
2003-04-25 16:21:20 +0000

    - Added the DTD, which must be manually moved to the right place at the moment: /boot/home/config/settings/Themis/dtd - Completely changed the HTMLParser addon to work with the new parser
    git-svn-id: svn://svn.code.sf.net/p/themis/code@200 ecaf47ba-bb13-0410-a3b6-899b003a5108

--- trunk/themis/modules/HTMLParser/HTMLParser.h
+++ trunk/themis/modules/HTMLParser/HTMLParser.h
@@ -20,13 +20,6 @@
 
 using namespace std;
 
-enum StringType	{
-	TEXT,
-	TAG,
-	ATTRVALUE,
-	ATTR
-};
-
 extern "C" __declspec( dllexport ) status_t Initialize( void * info = NULL );
 extern "C" __declspec( dllexport ) status_t Shutdown( bool now = false );
 extern "C" __declspec( dllexport ) PlugClass * GetObject();
@@ -38,117 +31,7 @@ class HTMLParser	:	public BHandler, public PlugClass	{
 		CachePlug * cache;
 		uint32 userToken;
 
-		// Parsing variables
-		unsigned int mPos;
-		unsigned int mOldPos; // Position in string before last read.
-		unsigned int mLineNr;
-		unsigned int mCharNr;
-		unsigned int mOldTagLineNr;
-		unsigned int mOldTagCharNr;
-		unsigned int mTagLineNr;
-		unsigned int mTagCharNr;
-		
-		bool mAttrNoValue; // Indicates if an tag attribute has a value
-		string mOldTag; // Previous tag
-		string mTag;	// Current tag
-		string mContent; // Content of the to be parsed file
-		StringType mStringType; // Type of the text last retrieved
-		bool mCloseTag; // Indicates if the current tag is a closing tag
 		TDocumentShared mDocument; // Stores the document to be build
-		// Functions for examining tags
-		inline bool isStartTag()	;
-		inline bool isDocTypeTag();
-		inline bool isHtmlTag();
-		inline bool isCommentTag();
-		inline bool isHeadTag();
-		inline bool isBodyTag();
-		inline bool isTitleTag();
-		inline bool isStyleTag();
-		inline bool isScriptTag();
-		inline bool isIsIndexTag();
-		inline bool isBaseTag();
-		inline bool isMetaTag();
-		inline bool isLinkTag();
-		inline bool isBlockLevelTag();
-		inline bool isTextLevelTag();
-		inline bool isHeadingTag();
-		inline bool isAdressTag();
-		inline bool isPTag();
-		inline bool isListTag();
-		inline bool isULTag();
-		inline bool isOLTag();
-		inline bool isDLTag();
-		inline bool isLITag();
-		inline bool isDDTag();
-		inline bool isDTTag();
-		inline bool isPreTag();
-		inline bool isBodyStyleTag();
-		inline bool isFormTag();
-		inline bool isEmptyBlockTag();
-		inline bool isTableTag();
-		inline bool isTrTag();
-		inline bool isCaptionTag();
-		inline bool isTdTag();
-		inline bool isThTag();
-		inline bool isFontStyleTag();
-		inline bool isFontStylePreTag();
-		inline bool isPhraseTag();
-		inline bool isFormFieldTag();
-		inline bool isInputTag();
-		inline bool isSelectTag();
-		inline bool isTextAreaTag();
-		inline bool isOptionTag();
-		inline bool isAnchorTag();
-		inline bool isEmptyTextTag();
-		inline bool isEmptyTextPreTag();
-		inline bool isAppletTag();
-		inline bool isParamTag();
-		inline bool isFontTag();
-		inline bool isMapTag();
-		inline bool isAreaTag();
-		// Functions for getting a part of the input
-		bool isWhiteSpace();
-		void nextChar();
-		string getTag();
-		string getText( bool aConserveSpaces = false );
-		string getAttrValue();
-		string getAttribute();
-		string getComment();
-		string getString( bool aConserveSpaces = false );
-		// Functions for moving around in the input
-		void skipTag(); // Skip the whole tag
-		void backPedal(); // Go back to to mOldPos
-		
-		// Tag recognition functions
-		void doctypeTag();
-		void htmlTag();
-		void commentTag( TElementShared aParent );
-		void headTag( TElementShared aParent );
-		void normalHeadTag( TElementShared aParent );
-		void bodyStyleTag( TElementShared aParent, bool aInsideForm = false );
-		void adressTag( TElementShared aParent );
-		void blockLevelTag( TElementShared aParent, bool aInsideForm = false );
-		void textLevelTag( TElementShared aParent, bool aConserveSpaces = false, bool aInsideAnchor = false );
-		void flowLevelTag( TElementShared aParent );
-		void formFieldTag( TElementShared aParent, bool aConserveSpaces = false );
-		void pTag( TElementShared aParent );
-		void listTag( TElementShared aParent );
-		void tableTag( TElementShared aParent );
-		void trTag( TElementShared aParent );
-		void preTag( TElementShared aParent );
-		void emptyElementTag( TElementShared aParent );
-		void selectTag( TElementShared aParent, bool aConserveSpaces = false );
-		void pcDataTag( TElementShared aParent, bool aConserveSpaces = false );
-		void appletTag( TElementShared aParent, bool aConserveSpaces = false, bool aInsideAnchor = false );
-		void mapTag( TElementShared aParent );
-		void normalTextTag( TElementShared aParent, bool aConserveSpaces = false, bool aInsideAnchor = false );
-	
-		// Main functions
-		void reset();
-		void showTree( const TNodeShared aNode, int aSpacing );
-		void showDocument();
-		void setContent( string aContent );
-		void startParsing( TDocumentShared aDocument );
 	
 	public:
 		HTMLParser( BMessage * info = NULL );
@@ -164,14 +47,6 @@ class HTMLParser	:	public BHandler, public PlugClass	{
 		status_t ReceiveBroadcast( BMessage * message );
 		int32 Type();
 		
-};
-
-class ReadException	{
-	
-	public:
-		ReadException()	{};
-		~ReadException()	{};
-
 };
 
 #endif
