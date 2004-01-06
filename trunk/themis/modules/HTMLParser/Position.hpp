586e2af15098477b7ed55e23fb70033c7500450b
mark_hellegers <mark_hellegers@ecaf47ba-bb13-0410-a3b6-899b003a5108>
2004-01-06 22:11:18 +0000

    - Documented a few helper classes of the HTMLParser.
    git-svn-id: svn://svn.code.sf.net/p/themis/code@302 ecaf47ba-bb13-0410-a3b6-899b003a5108

--- trunk/themis/modules/HTMLParser/Position.hpp
+++ trunk/themis/modules/HTMLParser/Position.hpp
@@ -1,9 +1,31 @@
-/*	Position
-	Stores the position in the document
-
-	Mark Hellegers (M.H.Hellegers@stud.tue.nl)
-	25-03-2003
+/*
+	Copyright (c) 2003 Mark Hellegers. All Rights Reserved.
 	
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom
+	the Software is furnished to do so, subject to the following
+	conditions:
+	
+	   The above copyright notice and this permission notice
+	   shall be included in all copies or substantial portions
+	   of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+	KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+	WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+	PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+	OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+	OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+	OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	
+	Original Author: 	Mark Hellegers (M.H.Hellegers@stud.tue.nl)
+	Project Start Date: October 18, 2000
+	Class Start Date: March 25, 2003
 */
 
 #ifndef POSITION_HPP
@@ -15,24 +37,94 @@
 // Namespaces used
 using namespace std;
 
+/// Stores the position in the document.
+
+/**
+	The Position class stores the position of the parser in a document.
+	It takes care of all things related to the position, including making
+	sure it stays inside the document.
+	It is possible to restrict the positions that can be accessed to allow
+	access to only a subpart of a document.
+*/
+
 class Position	{
 	
 	private:
+		/// The start of the piece of text to keep track of.
 		unsigned int mStart;
+		/// The end of the piece of text to keep track of.
 		unsigned int mEnd;
+		/// The current position in the document.
+		/**
+			The current position in the document from the beginning of the
+			document. It can only move between mStart and mEnd.
+		*/
 		unsigned int mIndex;
+		/// The current line number of the document.
 		unsigned int mLineNr;
+		/// The current character number of the document.
 		unsigned int mCharNr;
 		
 	public:
+		/// Constructor of the Position class.
+		/**
+			The constructor of the Position class. It sets up the necessary
+			range, size and start values.
+			
+			@param	aStart	The start of the piece of text to keep track of.
+			@param	aSize		The size of the piece of text to keep track of.
+			@param	aLineNr	The line number the piece of text starts at.
+			@param	aCharNr	The character number the piece of text starts at.
+		*/
 		Position( unsigned int aStart = 0, unsigned int aSize = 0,
 					 unsigned aLineNr = 1, unsigned int aCharNr = 1 );
+		/// Destructor of the Position class.
+		/**
+			The destructor of the Position class does nothing.
+			Everything is cleaned up automatically.
+		*/
 		~Position();
+		/// A function to get the index of the current position.
 		unsigned int getIndex() const;
+		/// A function to get the line number of the current position.
 		unsigned int getLineNr() const;
+		/// A function to get the character number of the current position.
 		unsigned int getCharNr() const;
+		/// A function to get the size of the piece of text to keep track of.
 		unsigned int getSize() const;
+		/// A function to go to the next position in the document.
+		/**
+			This function goes to the next position in the document.
+			If this would take it past the end of the piece of text it keeps
+			track of, it throws an exception. It keeps the line number and
+			character number up to date.
+			
+			@param	aText	The text to which the position belongs.
+			@return	The new position in the document.
+			
+			@exception	PositionException	Thrown if going to the next index
+															will cause it to go past the end
+															of the piece of text to keep
+															track of.
+			@todo	@li	Have to see if the return value is of any use.
+						@li	Have to see if it is not better to use a smart pointer
+								to store the text, instead of giving it as an argument
+								to this function each time.
+						@li	The tab size is hard set to 4. Dunno if that is good.
+		*/
 		unsigned int nextPosition( const string & aText );
+		/// A function to increase the size of the piece of text.
+		/**
+			This function increases the size of the piece of text allowing
+			a Position object to look at a larger part of a document.
+			It is primarily used when loading a new document and the
+			Position object already exists.
+			
+			@todo	@li	Have to see if this function is really necessary.
+						@li	Have to see if we can add a parameter to set
+								the size with which to increase.
+								It currently looks very inefficient.
+		*/
 		void increaseSize();
 
 };
--- trunk/themis/modules/HTMLParser/State.hpp
+++ trunk/themis/modules/HTMLParser/State.hpp
@@ -1,9 +1,31 @@
-/*	State
-	Stores the state of the document
-
-	Mark Hellegers (M.H.Hellegers@stud.tue.nl)
-	07-04-2003
+/*
+	Copyright (c) 2003 Mark Hellegers. All Rights Reserved.
 	
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom
+	the Software is furnished to do so, subject to the following
+	conditions:
+	
+	   The above copyright notice and this permission notice
+	   shall be included in all copies or substantial portions
+	   of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+	KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+	WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+	PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+	OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+	OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+	OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	
+	Original Author: 	Mark Hellegers (M.H.Hellegers@stud.tue.nl)
+	Project Start Date: October 18, 2000
+	Class Start Date: April 04, 2003
 */
 
 #ifndef STATE_HPP
@@ -18,14 +40,44 @@
 // Namespaces used
 using namespace std;
 
+/// Stores the state of a document.
+
+/**
+	This class stores the state of a document. A state is a set of positions
+	which are collected by a parser as it jumps to different parts
+	of the document. It is used to keep track of where in the document
+	the parser is and which way it has to go back.
+	
+	@todo Have to see if this class can't be used more to store
+			   lists of positions. Will probably have to expand on the
+			   functionality then.
+*/
+
 class State	{
 	
 	private:
+		/// List of positions stored so far.
+		/**
+			@todo	Might be better to use a stack for this.
+		*/
 		vector<Position> mPositions;
 		
 	public:
+		/// Constructor of the State class.
+		/**
+			The constructor of the State class.
+			It keeps a copy of the positions.
+			
+			@param	aPositions	The positions to store.
+		*/
 		State( const vector<Position> & aPositions );
+		/// Destructor of the State class.
+		/**
+			The destructor of the State class does nothing.
+			Everything is cleaned up automatically.
+		*/
 		~State();
+		/// A function to return the list of positions.
 		vector<Position> getPositions() const;
 
 };
