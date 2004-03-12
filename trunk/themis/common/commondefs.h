af4e230cfaad8b663c375bc412e9d037a79c3290
mweirauch <mweirauch@ecaf47ba-bb13-0410-a3b6-899b003a5108>
2004-03-12 22:34:19 +0000

    Some message constant changes. Michael
    git-svn-id: svn://svn.code.sf.net/p/themis/code@341 ecaf47ba-bb13-0410-a3b6-899b003a5108

--- trunk/themis/common/commondefs.h
+++ trunk/themis/common/commondefs.h
@@ -151,8 +151,8 @@ to allow interoperability in some parts of the application.
 #define UH_LOAD_NEW_PAGE		'ulnp'	// sent by win to UH/all interested
 #define UH_LOADING_FINISHED		'ulfi'	// sent by network to UH
 #define UH_LOADING_PROGRESS		'ulpg'	// sent by network to UH
-#define UH_PARSE_HTML_FINISHED	'uphf'	// sent by HTML parser to UH
-#define UH_PARSE_HTML_START		'uphs'	// sent by UH to HTML parser
+#define UH_PARSE_DOC_FINISHED	'updf'	// sent by HTML parser to UH
+#define UH_PARSE_DOC_START		'upds'	// sent by UH to HTML parser
 #define UH_RELOAD_PAGE			'urel'	// sent by win to UH/all interested
 #define UH_RENDER_START			'urst'	// sent by UH to renderer
 #define UH_RENDER_FINISHED		'urfi'	// snet by renderer to UH
