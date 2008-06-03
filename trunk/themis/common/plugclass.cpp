c8aff3e4c99462affe8162db189ff88238da5453
z3r0_one <z3r0_one@ecaf47ba-bb13-0410-a3b6-899b003a5108>
2008-06-03 13:09:01 +0000

    Updated an email address as a test to confirm that I have a subversion client configured properly.
    git-svn-id: svn://svn.code.sf.net/p/themis/code@447 ecaf47ba-bb13-0410-a3b6-899b003a5108

--- trunk/themis/common/plugclass.cpp
+++ trunk/themis/common/plugclass.cpp
@@ -23,7 +23,7 @@ OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-Original Author & Project Manager: Z3R0 One (z3r0_one@yahoo.com)
+Original Author & Project Manager: Z3R0 One (z3r0_one@users.sourceforge.net)
 Project Start Date: October 18, 2000
 */
 
