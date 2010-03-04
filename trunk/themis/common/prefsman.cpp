c674274122922c68c180aa3f83e56ef89e4f60ce
z3r0_one <z3r0_one@ecaf47ba-bb13-0410-a3b6-899b003a5108>
2010-03-04 15:33:07 +0000

    Some quick fixes for the prefsman.cpp to actually make it compile. It's still useless, mind you, but if you add it to the makefile, it will at least compile now.
    git-svn-id: svn://svn.code.sf.net/p/themis/code@538 ecaf47ba-bb13-0410-a3b6-899b003a5108

--- trunk/themis/common/prefsman.cpp
+++ trunk/themis/common/prefsman.cpp
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <StorageKit.h>
 #include "prefsman.h"
 
 PrefsManager::PrefsManager(const char *PREF_FILENAME, int32 FLAGS)
@@ -9,8 +10,8 @@ PrefsManager::PrefsManager(const char *PREF_FILENAME, int32 FLAGS)
 		//BEntry entry();
 		int32 length = strlen(PREF_FILENAME);
 		this->pref_filename = new char[length+1];
-		memset(this->pref_filename,0,length+1);
-		strncpy(this->pref_filename,PREF_FILENAME,length);
+		memset((char*)this->pref_filename,0,length+1);
+		strncpy((char*)this->pref_filename,PREF_FILENAME,length);
 	}
 }
 PrefsManager::PrefsManager(entry_ref *ref)
@@ -22,20 +23,20 @@ PrefsManager::~PrefsManager()
 	if( this->base_dir_entry != NULL ) delete this->base_dir_entry;
 	if( this->pref_filename != NULL)
 	{
-		memset(this->pref_filename,0,strlen(this->pref_filename)+1);
+		memset((char*)this->pref_filename,0,strlen(this->pref_filename)+1);
 		delete this->pref_filename;
 		this->pref_filename = NULL;
 	}
 }
 void PrefsManager::init(void)
 {
-	this->filename = NULL;
+	this->pref_filename = NULL;
 	BPath path;
 	if( find_directory(B_USER_SETTINGS_DIRECTORY,&path) == B_OK)
 	{
 		path.Append("Themis/",true);
 		this->base_dir_entry = new BEntry(path.Path(),true);
-		if( this->base_dir_entry.Exists() == false)
+		if( this->base_dir_entry->Exists() == false)
 		{
 			create_directory(path.Path(),0777);
 			
@@ -64,5 +65,5 @@ int32 PrefsManager::savePrefs(BMessage *PREFS)
 }
 const char *PrefsManager::filename(void)
 {
-	return this->filename;
+	return this->pref_filename;
 }
